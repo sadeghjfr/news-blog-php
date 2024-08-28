96b926e430f24c870ee2daf62221095e7196bdd4
sadeghjfr <sadeghjfr22@gmail.com>
2024-08-28 16:02:44 +0330

    setup database PDO connection

---
+++ functions/connection.php
@@ -0,0 +1,25 @@
+<?php
+
+global $conn;
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "news_blog_db";
+$dns = "mysql:host=$server_name;dbname=$db_name";
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+);
+
+try {
+
+    $conn = new PDO($dns, $user_name, $password, $options);
+
+    echo "Database connected!";
+
+    //$conn = null;
+}
+
+catch (PDOException $e){
+    echo $e->getMessage();
+}
