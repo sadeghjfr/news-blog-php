85a90da8d2a4747d3090e4fe6fb4bc37920dc89b
sadeghjfr <sadeghjfr22@gmail.com>
2024-08-28 16:47:30 +0330

    adding some helper functions

--- functions/helpers.php
+++ functions/helpers.php
@@ -1,1 +1,31 @@
 <?php
+
+const BASE_URL = 'http://localhost:63342/news-blog/';
+
+function redirect($url){
+
+    header('Location: ' . trim(BASE_URL, '/ ') . '/' . trim($url, '/ '));
+    exit();
+}
+
+// css, js, images
+function asset($file){
+
+    return trim(BASE_URL, '/ ') . '/' . trim($file, '/ ');
+
+}
+
+// links
+function url($url){
+
+    return trim(BASE_URL, '/ ') . '/' . trim($url, '/ ');
+
+}
+
+// dump & die
+function dd($result){
+
+    echo "<pre>";
+    var_dump($result);
+    exit();
+}
